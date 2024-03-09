0432198919073a830f0b05ed03b2881180d9ab43
ndluan <luandc1996n538@vlvh.ctu.edu.vn>
2024-03-09 10:46:09 +0700

    init code

---
+++ News/Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News.Models
+{
+    public class User
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public string Username {get; set;}
+        public string Password {get; set;}
+    
+    }
+}
